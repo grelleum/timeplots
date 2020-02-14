65972d16a35074b69dd3eef47cea70c8a92a2221
grelleum <greg.p.mueller@gmail.com>
2020-02-14 13:00:26 -0500

    from .timeplots import Plotter

--- timeplots/__init__.py
+++ timeplots/__init__.py
@@ -1,5 +1,9 @@
+# flake8: noqa: F401
+
 """Top-level package for timeplots."""
 
 __author__ = """Greg Mueller"""
-__email__ = 'greg@grelleum.com'
-__version__ = '0.1.0'
+__email__ = "greg@grelleum.com"
+__version__ = "0.1.0"
+
+from .timeplots import Plotter
