5f178064896f6ada1544941014e8c0bcb1f944c5
Greg Mueller <greg.p.mueller@gmail.com>
2020-02-19 09:46:22 -0500

    First working prototype, counts all lines.

--- timeplots/logplot.py
+++ timeplots/logplot.py
@@ -1,19 +1,113 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-from fileinput import FileInput
+"""
+logplot.py
 
+Usage:
+  logplot.py [options] [-e=<regex>...] <strptime> [<filename>...]
+  logplot.py -h | --help
+  logplot.py --version
+
+Options:
+  -h --help            Show this screen.
+  --version            Show version.
+  -e=<regex>           Pattern to match.
+  --hours=<hours>      Hours [default: 0].
+  --minutes=<minutes>  Minutes [default: 0].
+  --seconds=<seconds>  Seconds [default: 0].
+"""
+
+from collections import Counter, defaultdict, deque
+from datetime import datetime, timedelta
+from functools import lru_cache
+from fileinput import FileInput
+import re
 import sys
+
+from docopt import docopt
 
 import timeplots
 
-pattern = sys.argv[1]
 
-for line in FileInput(sys.argv[2:]):
-    try:
-        timestamp = timeplots.strptime(line, pattern)
-    except ValueError:
-        pass
-    else:
-        print(">>>", repr(timestamp))
+class LogTime(object):
+    def __init__(self, *, pattern, delimiter=" ", hours=0, minutes=0, seconds=0):
+        """
+        Creates an object that produces datetime objects.
+        strptime: str: strptime pattern for decoding embedded timestamp.
+        hours: int: used to group timestamp by hour.
+        minutes: int: used to group timestamp by minutes.
+        seconds: int: used to group timestamp by seconds.
+        """
+        self.pattern = pattern
+        self.pattern_size = len(pattern.split())
+        self.delimiter = delimiter
+        delta = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        self.delta = int(delta.total_seconds())
 
+    @lru_cache()
+    def _strptime(self, text):
+        timestamp = datetime.strptime(text, self.pattern)
+        if self.delta:
+            big_delta = timestamp - datetime.min
+            total_seconds = int(big_delta.total_seconds())
+            mod = timedelta(seconds=total_seconds % self.delta)
+            timestamp = timestamp - mod
+        return timestamp
+
+    @lru_cache()
+    def strptime(self, text):
+        words = text.split(self.delimiter)
+        words = words[: self.pattern_size]
+        text = " ".join(words)
+        # print("***", text, flush=True, end="\t")
+        timestamp = self._strptime(text)
+        return timestamp
+
+
+def match_any_lines(logtime, lines):
+    for line in lines:
+        try:
+            timestamp = logtime.strptime(line)
+        except ValueError as e:
+            print(repr(e), file=sys.stderr)
+        else:
+            print(">>>", timestamp, end="\r", flush=True)
+            yield timestamp
+
+
+# def match_lines(logtime, lines, expressions):
+#     expressions = [re.compile(expression) for expression in expressions]
+#     for line in lines:
+#         for expression in expressions:
+#             try:
+#                 timestamp = logtime.strptime(line)
+#             except ValueError as e:
+#                 print(repr(e), file=sys.stderr)
+#             else:
+#                 print(">>>", timestamp, end="\r", flush=True)
+#                 yield timestamp
+
+
+def main():
+    args = docopt(__doc__)
+    print(args)
+
+    logtime = LogTime(
+        pattern=args.get("<strptime>"),
+        hours=int(args.get("--hours")),
+        minutes=int(args.get("--minutes")),
+        seconds=int(args.get("--seconds")),
+    )
+
+    lines = (line for line in FileInput(args.get("<filename>")))
+    c = Counter(match_any_lines(logtime, lines))
+    times, data = zip(*sorted(c.items()))
+
+    plotter = timeplots.Plotter(width=1400)
+    plotter.new_plot("Stuff happening", "stuff/day")
+    plotter.add_line(f"stuff/day", times, data)
+    plotter.render(filename="testplots.html", title="testplots")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
