8dd188876bb0e0a52d5ea4d27a9302f5aa324168
Greg Mueller <Greg.Mueller@riverbed.com>
2020-06-06 14:05:57 -0400

    Display version on command line import.

---
+++ timeplots/__main__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Provide version information when imported on command line."""
+
+import sys
+import timeplots
+
+
+if __name__ == "__main__":
+    line = "{:14} {:>8}"
+    python_version = ".".join(str(x) for x in sys.version_info[:3])
+    print(line.format("Python", python_version))
+    print(line.format("Timeplots", timeplots.__version__))
