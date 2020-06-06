7e5e3d1056c98d0e51be93f24e9241b676bb608d
Greg Mueller <Greg.Mueller@riverbed.com>
2020-06-06 19:49:57 -0400

    Fixed errant '*' in missing_time_data.

--- timeplots/timeplots.py
+++ timeplots/timeplots.py
@@ -215,9 +215,9 @@ class TimeParser(object):
         )
 
 
-def missing_time_data(timestamps, *data, *, default=float("nan")):
+def missing_time_data(timestamps, data, *, default=0):
     """
-    Fill in missing times with a default value or "not a number".
+    Fill in missing times with a default value, usually zero.
 
     timestamps is a sequence of times in the datetime format.
     data is a sequence of values matching those timestamps.
