2b8d24b867c75d9138ade9ef63218e911fad51d9
Greg Mueller <Greg.Mueller@riverbed.com>
2020-06-06 14:17:39 -0400

    Initial commit.

---
+++ bump_version.py
@@ -0,0 +1,30 @@
+"""
+bump_version.py
+
+Change version number in this file and execute.
+"""
+
+import re
+
+version = "0.2.0"
+print('"version: {}"'.format(version))
+
+
+py_regex = re.compile(r"version = \"\d+\.[\d\.]+[a-z]?\"")
+py_replacement = 'version = "{}"'.format(version)
+
+
+def bump_version_in_file(filename, regex, replacement):
+    """Replace version number within a file."""
+    with open(filename, "rt") as fh:
+        contents = fh.read()
+    contents = regex.sub(replacement, contents)
+    with open(filename, "wt") as fh:
+        fh.write(contents)
+
+
+# bump_version_in_file("docs/index.rst", py_regex, py_replacement)
+bump_version_in_file("README.md", py_regex, py_replacement)
+bump_version_in_file("setup.py", py_regex, py_replacement)
+bump_version_in_file("timeplots/timeplots.py", py_regex, py_replacement)
+# bump_version_in_file("timeplots/about.py", py_regex, py_replacement)
